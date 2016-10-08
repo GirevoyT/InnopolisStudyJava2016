af77c7487d0c32f84ef92d3615ef80c0138983e1
GirevoyT <eMPekEd2Yo7z>
2016-10-08 17:48:26 +0300

    Какаято бубуйня с русурсами была

---
+++ src/ru/innopolis/course/java2016/girevoy/home/homeworks/lab1/resource/Resource.java
@@ -0,0 +1,31 @@
+package ru.innopolis.course.java2016.girevoy.home.homeworks.lab1.resource;
+
+/**
+ * Created by Arxan on 08.10.2016.
+ */
+public abstract class Resource<T> {
+	/**
+	 * Этот мерод возвращает true если есть следующий объект для возврата методом next()
+	 * @return
+	 */
+	public abstract boolean hasNext();
+
+	/**
+	 * Этот метод возвращает следующий объект из ресурса
+	 * @return
+	 */
+	public abstract T next();
+	private volatile boolean isComplite; //WARNING! Зачем нам не волатайл переменные
+
+	/**
+	 * Этот метод возвращает статус: обработан ли реурс полностью
+	 * @return
+	 */
+	public boolean isComplite() {
+		return isComplite;
+	}
+
+	public void compliteResource() {
+		isComplite = true;
+	}
+}
