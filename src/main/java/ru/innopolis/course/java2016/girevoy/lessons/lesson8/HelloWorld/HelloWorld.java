b7c0c90f3966555cf9fbea58cd835f1b4e3bf5e1
masterlomaster <Arxangel_92@list.ru>
2016-10-12 11:41:17 +0300

    Добавил файл настроек для log4j

--- src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson8/HelloWorld/HelloWorld.java
+++ src/main/java/ru/innopolis/course/java2016/girevoy/lessons/lesson8/HelloWorld/HelloWorld.java
@@ -1,5 +1,6 @@
 package ru.innopolis.course.java2016.girevoy.lessons.lesson8.HelloWorld;
 
+import org.jcp.xml.dsig.internal.MacOutputStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,6 +10,16 @@ import org.slf4j.LoggerFactory;
 public class HelloWorld {
 	private static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 	public static void main(String[] args) {
-		logger.info("Тестовое сообщене для логера");
+		if (logger.isInfoEnabled()) {
+			Integer i = (int)(Math.random() * 1000);
+			logger.info("Тестовое сообщене для логера. Число {}",i);
+		}
+		logger.debug("Тестовое сообщение для логгера. Дебаг");
+
+
+		logger.error("error Test");
+		logger.info("info Test");
+		logger.debug("debugg Test");
+		logger.trace("trace Test");
 	}
 }
